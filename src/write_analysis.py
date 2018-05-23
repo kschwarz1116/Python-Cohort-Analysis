b8b64b22b4710adf237b82707c1ee4ddb4681295
Kevin Schwarz <kschwarz1116@gmail.com>
2018-05-23 14:05:00 -0500

    Working analysis

--- src/write_analysis.py
+++ src/write_analysis.py
@@ -22,22 +22,25 @@ def generate_cohort_analysis(customers: Dict[int, Customer]) -> List[Tuple[dt.da
     #Find cohort bounds
     earliest_created = customers[min(customers, key=lambda x: customers[x].created)].created
     latest_created = customers[max(customers, key=lambda x: customers[x].created)].created
-    
+
     #Initialize output array
     cohort = latest_created
-    while earliest_created < cohort:
+    while earliest_created <= cohort:
         cohorts.append((cohort, 0, []))
         cohort = cohort - dt.timedelta(7)
     
-    for customer in customers:
+    for customer in customers.values():
         add_to_cohorts(customer, cohorts)
 
     return cohorts
 
 def add_to_cohorts(customer: Customer, cohorts: List[Tuple[dt.datetime, int, List[int]]]) -> None:
-    #Figure out which cohort
     cohort = (cohorts[0][0] - customer.created) // dt.timedelta(7)
-    #Figure out how to modify cohorts[cohort][2]
+
+    datetime, num_customers, distinct_purchases = cohorts[cohort]
+    
+    num_customers += 1
+
     distinct_purchase_weeks = set()
     max_purchase_week = 0
 
@@ -46,11 +49,14 @@ def add_to_cohorts(customer: Customer, cohorts: List[Tuple[dt.datetime, int, Lis
         distinct_purchase_weeks.add(purchase_week)
         max_purchase_week = max(max_purchase_week, purchase_week)
 
-    if (len(cohorts[cohort][2]) < max_purchase_week + 1):
-        cohorts[cohort][2].extend([0]*(max_purchase_week + 1 - len(cohorts[cohort][2])))
+    if (len(distinct_purchases) < max_purchase_week + 1):
+        distinct_purchases.extend([0]*(max_purchase_week + 1 - len(distinct_purchases)))
     
     for purchase_week in distinct_purchase_weeks:
         cohorts[cohort][2][purchase_week] += 1
+
+    
+    cohorts[cohort] = (datetime, num_customers, distinct_purchases)
     return
 
 def print_cohort_analysis(cohort: List[Tuple[dt.datetime, int, List[int]]], out_file: str) -> None:
