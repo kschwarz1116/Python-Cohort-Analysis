9b6ea05129c7aa400b43530ceaf99356a7c7986c
Kevin Schwarz <kschwarz1116@gmail.com>
2018-05-25 00:19:15 -0500

    Grab command line input properly

--- src/main.py
+++ src/main.py
@@ -13,14 +13,17 @@ def main() -> None:
     """This function accepts command line arguments and parses them into a table"""
 
     num_args: int = len(sys.argv)
-    if (num_args < 4) or (num_args > 6):
+    if (num_args < 4) or (num_args > 5):
         print("Got improper args")
         return
 
     customer_file: str = sys.argv[1]
     order_file: str = sys.argv[2]
     out_file: str = sys.argv[3]
-    offset_hours: str = sys.argv[4]
+    if num_args == 5:
+        offset_hours: str = sys.argv[4]
+    else:
+        offset_hours = "0"
 
     customers: CUSTOMERS = read_customer.read_customers(customer_file)
     read_orders.read_orders(order_file, customers)
